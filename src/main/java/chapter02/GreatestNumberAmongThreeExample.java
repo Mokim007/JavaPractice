350090bddb2d1fd8264322e56bea0bbed3b1e256
Mokim007 <ansarimdmokim2019@gmail.com>
2022-03-06 20:39:44 +0530

    Greatest Number

---
+++ src/main/java/chapter02/GreatestNumberAmongThreeExample.java
@@ -0,0 +1,20 @@
+package chapter02;
+
+public class GreatestNumberAmongThreeExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x,y,z;
+		x=500;
+		y=300;
+		z=600;
+		if(x>y & x>z)
+			System.out.println(x +" x is greates");
+		else if (y>z)
+			System.out.println(y +" y is greates");
+		else
+			System.out.println(z +" z is greates");
+		
+	}
+
+}
